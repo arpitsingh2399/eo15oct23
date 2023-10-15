efa8e62e03f37e3554ae552f6feac0f62f6491cf
arpitsingh2399 <70560943+arpitsingh2399@users.noreply.github.com>
2023-10-15 14:56:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql2");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const con = mysql.createConnection({
-host :"sql12.freesqldatabase.com",
-user :"sql12651861",
-password :"WGjaWRsVTl",
-database : "sql12651861"
-});
+app.post("/find" , (req,res) =>
+{
+let num = parseInt(req.body.num);
 
-app.post("/save" , (req,res) => {
-
-let data = [ req.body.name , req.body.feedback ] ;
-let sql = "insert into feedback values(?,?)";
-
-con.query(sql,data, (err,result) => {
-if(err) return res.send(err);
-else return res.send(result);
+let ans = num % 2 == 0 ? "even" : "odd" ;
+res.send(ans);
 
 
 
@@ -29,7 +19,4 @@ else return res.send(result);
 
 
 
-})
-
-
-app.listen(9000 , () => { console.log("ready to serve @9000");} );
+app.listen(9000, () => { console.log("ready @9000") ; }) ;
